b9aa761da6e02ae6f1f898f4d9a85fbd9ccf4f9f
Cillié Malan <cilliemalan@gmail.com>
2019-03-05 13:47:18 +0200

    GMac commented out debugs

--- MicroRatchet/GMac.cs
+++ MicroRatchet/GMac.cs
@@ -4,6 +4,7 @@ using Org.BouncyCastle.Crypto.Modes;
 using Org.BouncyCastle.Crypto.Parameters;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace MicroRatchet
@@ -16,6 +17,8 @@ namespace MicroRatchet
         {
             byte[] output = new byte[_gmac.GetMacSize()];
             _gmac.DoFinal(output, 0);
+            //Debug.WriteLine($"   OUTPUT:  {Convert.ToBase64String(output ?? new byte[0])}");
+            //Debug.WriteLine($"--GMAC--");
             return output;
         }
 
@@ -23,11 +26,16 @@ namespace MicroRatchet
         {
             _gmac = new Org.BouncyCastle.Crypto.Macs.GMac(new GcmBlockCipher(new AesEngine()), macSize);
             _gmac.Init(new ParametersWithIV(new KeyParameter(key), iv));
+            //Debug.WriteLine($"--GMAC--");
+            //Debug.WriteLine($"   KEY:     {Convert.ToBase64String(key)}");
+            //Debug.WriteLine($"   NONCE:   {Convert.ToBase64String(iv ?? new byte[0])}");
+            //Debug.WriteLine($"   SIZE:    {macSize}");
         }
 
         public void Process(ArraySegment<byte> data)
         {
             _gmac.BlockUpdate(data.Array, data.Offset, data.Count);
+            //Debug.WriteLine($"   PAYLOAD: {Convert.ToBase64String(data.Array, data.Offset, data.Count)}");
         }
     }
 }
