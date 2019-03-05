88d6da7049ec59bcea3d9695db3a0f9d9958aa61
Cillié Malan <cilliemalan@gmail.com>
2019-03-05 20:32:55 +0200

    Added C project and tests

---
+++ libmicroratchet/kdf.c
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+int test()
+{
+	return 5;
+}
---
+++ microratchettests/test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include <microratchet.h>
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(5, test());
+  EXPECT_TRUE(true);
+}
