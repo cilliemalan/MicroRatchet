5d8bbc41b4100c95f6d26ea8882bd90decaffc5b
Cillié Malan <cilliemalan@gmail.com>
2019-03-09 20:11:43 +0200

    AES 128 now default

--- MicroRatchet/MicroRatchetConfiguration.cs
+++ MicroRatchet/MicroRatchetConfiguration.cs
@@ -9,7 +9,7 @@ namespace MicroRatchet
 
         public int Mtu { get; set; } = 1000;
         public bool IsClient { get; set; }
-        public bool UseAes256 { get; set; } = true;
+        public bool UseAes256 { get; set; } = false;
         public int NumberOfRatchetsToKeep { get; set; } = 7;
         public int MaxLostKeys { get; set; } = 100;
     }
