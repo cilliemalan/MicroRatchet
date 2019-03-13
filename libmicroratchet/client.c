3c13fc5d3c4ba30147734a9d1811d5e94152150e
Cillié Malan <cilliemalan@gmail.com>
2019-03-13 10:50:26 +0200

    client now has its own sha

--- libmicroratchet/client.c
+++ libmicroratchet/client.c
@@ -6,6 +6,8 @@ mr_ctx mrclient_create(mr_config* config)
 {
 	_mr_ctx* ctx;
 	int r = mr_allocate(0, sizeof(_mr_ctx), &ctx);
+	memset(ctx, 0, sizeof(_mr_ctx));
+	ctx->sha_ctx = mr_sha_create(ctx);
 	if (r != E_SUCCESS) return 0;
 	return ctx;
 }
@@ -25,10 +27,16 @@ int mrclient_send_data(mr_ctx ctx, const unsigned char* data, int numbytes)
 	return E_INVALIDARGUMENT;
 }
 
-void mrclient_destroy(mr_ctx ctx)
+void mrclient_destroy(mr_ctx _ctx)
 {
+	_mr_ctx* ctx = _ctx;
 	if (ctx)
 	{
+		if (ctx->sha_ctx)
+		{
+			mr_sha_destroy(ctx->sha_ctx);
+			ctx->sha_ctx = 0;
+		}
 		mr_free(ctx, ctx);
 	}
 }
