53fec0c904e8fa279b5e9a0702ff7f00774f5ac2
Cillié Malan <cilliemalan@gmail.com>
2019-03-14 21:46:38 +0200

    Implemented ecdh generate

--- microratchetwolfssl/ecdh.c
+++ microratchetwolfssl/ecdh.c
@@ -1,15 +1,6 @@
 #include "pch.h"
 #include <microratchet.h>
-
-#define HAVE_ECC
-#define HAVE_ECC_SIGN
-#define HAVE_ECC_VERIFY
-#define HAVE_ECC_DHE
-#define HAVE_ECC_KEY_IMPORT
-#define HAVE_ECC_KEY_EXPORT
-#define ECC_SHAMIR
-#define HAVE_COMP_KEY
-#define USE_ECC_B_PARAM
+#include <wolfssl/wolfcrypt/random.h>
 #include <wolfssl/wolfcrypt/ecc.h>
 
 
@@ -36,12 +27,31 @@ int mr_ecdh_generate(mr_ecdh_ctx _ctx, unsigned char* publickey, unsigned int pu
 	if (!publickey || !ctx || !publickeysize) return E_INVALIDARGUMENT;
 
 	WC_RNG rng;
-	wc_InitRng(&rng);
-	int result = wc_ecc_make_key(&rng, 32, &ctx->key);
+	int result = wc_InitRng(&rng);
 	if (result != 0) return E_INVALIDOP;
-	//mr_ecdh_generate_cb(E_SUCCESS, ctx, ctx->mr_ctx);
+	int pubkeylen = 0;
+	for (;;)
+	{
+		result = wc_ecc_make_key_ex(&rng, 32, &ctx->key, ECC_SECP256R1);
+		if (result != 0) return E_INVALIDOP;
 
+		// try until we have a private key with an even public key y component.
+		if (mp_iseven(ctx->key.pubkey.y))
+		{
+			// try until the public key x component is under or at 256 bits.
+			pubkeylen = mp_unsigned_bin_size(ctx->key.pubkey.x);
+			if (pubkeylen <= 32)
+			{
+				break;
+			}
+		}
+	}
+
+	memset(publickey, 0, 32);
+	result = mp_to_unsigned_bin(ctx->key.pubkey.x, publickey + (32 - pubkeylen));
+	if (result != 0) return E_INVALIDOP;
 	*publickeysize = 32;
+	mr_ecdh_generate_cb(E_SUCCESS, ctx, ctx->mr_ctx);
 	return E_SUCCESS;
 }
 
