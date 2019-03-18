027e5e5b68b38a52004485d8c0cab51e9f4ce83e
Cillié Malan <cilliemalan@gmail.com>
2019-03-18 15:42:34 +0200

    Refactor ecdsa

--- microratchetwolfssl/ecdsa.c
+++ microratchetwolfssl/ecdsa.c
@@ -45,15 +45,11 @@ int mr_ecdsa_load(mr_ecdsa_ctx _ctx, unsigned char* data, unsigned int spaceavai
 int mr_ecdsa_sign(mr_ecdsa_ctx _ctx, const unsigned char* digest, unsigned int digestsize, unsigned char* signature, unsigned int signaturespaceavail, unsigned int* signaturesize)
 {
 	_mr_ecdsa_ctx* ctx = _ctx;
+	if (!ctx || !digest || !signature || !signaturesize) return E_INVALIDARG;
+	if (signaturespaceavail < 64) return E_INVALIDSIZE;
 
-	// why????
-	WC_RNG rng;
-	int result = wc_InitRng(&rng);
-	if (result != 0) return E_INVALIDOP;
-
-	result = wc_ecc_sign_hash(digest, digestsize, signature, signaturesize, &rng, &ctx->key);
-	if (result != 0) return E_INVALIDOP;
-
+	int result = ecc_sign(&ctx->key, digest, digestsize, signature, signaturespaceavail, signaturesize);
+	if (result != E_SUCCESS) return result;
 	mr_ecdsa_sign_cb(E_SUCCESS, ctx, ctx->mr_ctx);
 	return E_SUCCESS;
 }
@@ -61,10 +57,11 @@ int mr_ecdsa_sign(mr_ecdsa_ctx _ctx, const unsigned char* digest, unsigned int d
 int mr_ecdsa_verify(mr_ecdsa_ctx _ctx, const unsigned char* signature, unsigned int signaturesize, const unsigned char* digest, unsigned int digestsize, unsigned int* result)
 {
 	_mr_ecdsa_ctx* ctx = _ctx;
+	if (!ctx || !digest || !signature || !signaturesize) return E_INVALIDARG;
+	if (signaturesize != 64) return E_INVALIDSIZE;
 
-	int res =  wc_ecc_verify_hash(signature, signaturesize, digest, digestsize, result, &ctx->key);
-	if (res != 0) return E_INVALIDOP;
-
+	int res = ecc_verify(&ctx->key, signature, signaturesize, digest, digestsize, result);
+	if (res != E_SUCCESS) return res;
 	mr_ecdsa_verify_cb(E_SUCCESS, ctx, ctx->mr_ctx);
 	return E_SUCCESS;
 }
@@ -81,6 +78,7 @@ int mr_ecdsa_store(mr_ecdsa_ctx _ctx, unsigned char* data, unsigned int spaceava
 	int len = ecc_store_size_needed(&ctx->key.k);
 	if (len < 0 || (unsigned int)len > spaceavail) return E_INVALIDSIZE;
 	int result = ecc_store(&ctx->key, data, spaceavail, amountstored);
+	if (result != E_SUCCESS) return result;
 	mr_ecdh_store_cb(E_SUCCESS, ctx, ctx->mr_ctx);
 	return E_SUCCESS;
 }
@@ -105,8 +103,8 @@ int mr_ecdsa_verify_other(const unsigned char* signature, unsigned int signature
 	res = ecc_import_public(publickey, publickeysize, &key.pubkey);
 	if (res != 0) return E_INVALIDOP;
 
-	res = wc_ecc_verify_hash(signature, signaturesize, digest, digestsize, result, &key);
-	if (res != 0) return E_INVALIDOP;
+	res = ecc_verify(&key, signature, signaturesize, digest, digestsize, result);
+	if (res != E_SUCCESS) return res;
 
 	mr_ecdsa_verify_other_cb(E_SUCCESS, user);
 	return E_SUCCESS;
