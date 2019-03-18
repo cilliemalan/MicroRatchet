52d5448458207c9d69052136469bd283c94a5731
Cillié Malan <cilliemalan@gmail.com>
2019-03-18 14:20:15 +0200

    Created EXPECT versions of buffer assertions

--- microratchettests/support.h
+++ microratchettests/support.h
@@ -54,3 +54,30 @@ inline void buffer_to_string(const unsigned char* b1, unsigned int l1, char* out
 	buffer_to_string(__b2, __l2, sbuffer2); \
 	ASSERT_STRNE(sbuffer1, sbuffer2); \
 }
+
+
+#define EXPECT_BUFFEREQ(b1, l1, b2, l2) { \
+	unsigned int __l1 = static_cast<unsigned int>(l1); \
+	unsigned int __l2 = static_cast<unsigned int>(l2); \
+	const unsigned char* __b1 = static_cast<const unsigned char*>(b1); \
+	const unsigned char* __b2 = static_cast<const unsigned char*>(b2); \
+	EXPECT_EQ(__l1, __l2); \
+	char *sbuffer1 = new char[l1*2+1]; \
+	char *sbuffer2 = new char[l1*2+1]; \
+	buffer_to_string(__b1, __l1, sbuffer1); \
+	buffer_to_string(__b2, __l2, sbuffer2); \
+	EXPECT_STREQ(sbuffer1, sbuffer2); \
+}
+
+#define EXPECT_BUFFERNE(b1, l1, b2, l2) { \
+	unsigned int __l1 = static_cast<unsigned int>(l1); \
+	unsigned int __l2 = static_cast<unsigned int>(l2); \
+	const unsigned char* __b1 = static_cast<const unsigned char*>(b1); \
+	const unsigned char* __b2 = static_cast<const unsigned char*>(b2); \
+	EXPECT_EQ(__l1, __l2); \
+	char *sbuffer1 = new char[l1*2+1]; \
+	char *sbuffer2 = new char[l1*2+1]; \
+	buffer_to_string(__b1, __l1, sbuffer1); \
+	buffer_to_string(__b2, __l2, sbuffer2); \
+	EXPECT_STRNE(sbuffer1, sbuffer2); \
+}
