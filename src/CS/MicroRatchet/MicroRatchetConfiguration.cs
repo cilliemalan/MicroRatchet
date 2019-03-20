028dc0f4e0b5c525c8b24f7b4c48639e232f3fe1
Cillié Malan <cilliemalan@gmail.com>
2019-03-20 12:40:35 +0200

    Config defaults to client

--- src/CS/MicroRatchet/MicroRatchetConfiguration.cs
+++ src/CS/MicroRatchet/MicroRatchetConfiguration.cs
@@ -8,7 +8,7 @@ namespace MicroRatchet
     {
 
         public int Mtu { get; set; } = 1000;
-        public bool IsClient { get; set; }
+        public bool IsClient { get; set; } = true;
         public bool UseAes256 { get; set; } = false;
         public int NumberOfRatchetsToKeep { get; set; } = 7;
         public int MaxLostKeys { get; set; } = 100;
