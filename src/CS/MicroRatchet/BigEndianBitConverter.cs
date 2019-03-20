256fa5488524435d29b98cf09748135c62ea2621
Cillié Malan <cilliemalan@gmail.com>
2019-03-20 10:21:54 +0200

    Added 16 bit big endian converters

--- src/CS/MicroRatchet/BigEndianBitConverter.cs
+++ src/CS/MicroRatchet/BigEndianBitConverter.cs
@@ -21,6 +21,17 @@ namespace MicroRatchet
             (byte) ((value >> 8) & 0xFF),
             (byte) (value & 0xFF),
         };
+        public static byte[] GetBytes(short value) => new byte[]
+        {
+            (byte) ((value >> 8) & 0xFF),
+            (byte) (value & 0xFF),
+        };
+
+        public static byte[] GetBytes(ushort value) => new byte[]
+        {
+            (byte) ((value >> 8) & 0xFF),
+            (byte) (value & 0xFF),
+        };
 
         public static int ToInt32(byte[] value, int offset = 0) =>
                 (value[offset + 0] << 24) |
@@ -33,5 +44,13 @@ namespace MicroRatchet
                 (value[offset + 1] << 16) |
                 (value[offset + 2] << 8) |
                 value[offset + 3]);
+
+        public static int ToInt16(byte[] value, int offset = 0) =>
+                (value[offset + 0] << 8) |
+                value[offset + 1];
+
+        public static uint ToUInt16(byte[] value, int offset = 0) =>
+            (uint)((value[offset + 0] << 8) |
+                value[offset + 1]);
     }
 }
