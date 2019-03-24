278b4718c2dd18a646227e0c9071b94288432ad8
Cillié Malan <cilliemalan@gmail.com>
2019-03-24 15:16:18 +0200

    Commented out debug lines for poly

--- src/CS/MicroRatchet/Poly.cs
+++ src/CS/MicroRatchet/Poly.cs
@@ -1,6 +1,7 @@
 ﻿using Org.BouncyCastle.Crypto.Parameters;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace MicroRatchet
@@ -14,28 +15,38 @@ namespace MicroRatchet
         {
             var mac = new byte[_poly.GetMacSize()];
             _poly.DoFinal(mac, 0);
+            
 
             if (_macSize == mac.Length)
             {
+                //Debug.WriteLine($"   MAC:     {Convert.ToBase64String(mac)}");
+                //Debug.WriteLine($"--maccing--");
                 return mac;
             }
             else
             {
                 var output = new byte[_macSize];
                 Array.Copy(mac, output, output.Length);
+                //Debug.WriteLine($"   MAC:     {Convert.ToBase64String(output)}");
+                //Debug.WriteLine($"--maccing--");
                 return output;
             }
         }
 
         public void Init(byte[] key, byte[] iv, int macSize)
         {
+            if (iv != null) throw new InvalidOperationException("IV not supported.");
             _macSize = macSize / 8;
             _poly = new Org.BouncyCastle.Crypto.Macs.Poly1305();
             _poly.Init(new KeyParameter(key));
+
+            //Debug.WriteLine($"--maccing--");
+            //Debug.WriteLine($"   KEY:     {Convert.ToBase64String(key)}");
         }
 
         public void Process(ArraySegment<byte> data)
         {
+            //Debug.WriteLine($"   MAC INPUT:     {Convert.ToBase64String(data.Array, data.Offset, data.Count)}");
             _poly.BlockUpdate(data.Array, data.Offset, data.Count);
         }
     }
