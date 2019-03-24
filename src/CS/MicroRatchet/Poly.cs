9abc8b0045248209324dec5d7201769a342aa0ba
Cillié Malan <cilliemalan@gmail.com>
2019-03-24 13:45:16 +0200

    Added Poly1305 MAC

---
+++ src/CS/MicroRatchet/Poly.cs
@@ -0,0 +1,42 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRatchet
+{
+    internal class Poly : IMac
+    {
+        private int _macSize;
+        private Org.BouncyCastle.Crypto.Macs.Poly1305 _poly;
+
+        public byte[] Compute()
+        {
+            var mac = new byte[_poly.GetMacSize()];
+            _poly.DoFinal(mac, 0);
+
+            if (_macSize == mac.Length)
+            {
+                return mac;
+            }
+            else
+            {
+                var output = new byte[_macSize];
+                Array.Copy(mac, output, output.Length);
+                return output;
+            }
+        }
+
+        public void Init(byte[] key, byte[] iv, int macSize)
+        {
+            _macSize = macSize;
+            _poly = new Org.BouncyCastle.Crypto.Macs.Poly1305();
+            _poly.Init(new KeyParameter(key));
+        }
+
+        public void Process(ArraySegment<byte> data)
+        {
+            _poly.BlockUpdate(data.Array, data.Offset, data.Count);
+        }
+    }
+}
