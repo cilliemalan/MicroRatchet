f7e962d635087d4fd3f351654094598a5e14d5e5
Cillié Malan <cilliemalan@gmail.com>
2019-04-23 14:42:43 +0200

    Added stub C++ tests for ratchets

---
+++ src/C/microratchettests/ecdhratchet.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include <microratchet.h>
+#include <internal.h>
+#include "support.h"
+
+TEST(EcdhRatchet, Basic) {
+}
---
+++ src/C/microratchettests/symmetricratchet.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include <microratchet.h>
+#include <internal.h>
+#include "support.h"
+
+TEST(SymmetricRatchet, Basic) {
+}
