815900e9b20d5a78551333da44c3eae35e954f55
Cillié Malan <cilliemalan@gmail.com>
2019-10-01 12:26:59 +0200

    Fixed ecc signature return types

--- src/C/microratchetwolfssl/ecc_common.h
+++ src/C/microratchetwolfssl/ecc_common.h
@@ -1,9 +1,9 @@
 #pragma once
 
-int ecc_import_public(const uint8_t* otherpublickey, uint32_t otherpublickeysize, ecc_point *pub);
-int ecc_generate(ecc_key* key, uint8_t* publickey, uint32_t publickeyspaceavail);
-int ecc_load(ecc_key* key, const uint8_t* data, uint32_t spaceavail);
-int ecc_store_size_needed(const mp_int* key);
-int ecc_store(const ecc_key* key, uint8_t* data, uint32_t spaceavail);
-int ecc_sign(const ecc_key *key, const uint8_t* digest, uint32_t digestsize, uint8_t* signature, uint32_t signaturespaceavail);
-int ecc_verify(const ecc_key *key, const uint8_t* signature, uint32_t signaturesize, const uint8_t* digest, uint32_t digestsize, uint32_t* result);
+mr_result_t ecc_import_public(const uint8_t* otherpublickey, uint32_t otherpublickeysize, ecc_point *pub);
+mr_result_t ecc_generate(ecc_key* key, uint8_t* publickey, uint32_t publickeyspaceavail);
+mr_result_t ecc_load(ecc_key* key, const uint8_t* data, uint32_t spaceavail);
+mr_result_t ecc_store_size_needed(const mp_int* key);
+mr_result_t ecc_store(const ecc_key* key, uint8_t* data, uint32_t spaceavail);
+mr_result_t ecc_sign(const ecc_key *key, const uint8_t* digest, uint32_t digestsize, uint8_t* signature, uint32_t signaturespaceavail);
+mr_result_t ecc_verify(const ecc_key *key, const uint8_t* signature, uint32_t signaturesize, const uint8_t* digest, uint32_t digestsize, uint32_t* result);
