5f90c916701aa52a1ebd2fe4f3ddced6f1b73014
Cillié Malan <cilliemalan@gmail.com>
2019-10-06 20:25:59 +0200

    Added custom RNG functionality for reference tests

--- src/C/microratchetwolfssl/rng.c
+++ src/C/microratchetwolfssl/rng.c
@@ -2,10 +2,32 @@
 #include <microratchet.h>
 #include <wolfssl/wolfcrypt/random.h>
 
+#ifdef CUSTOM_RNG
+extern const uint8_t* random_data;
+extern const uint32_t random_data_length;
+#endif
+
 typedef struct {
 	mr_ctx mr_ctx;
 	WC_RNG rng;
+#ifdef CUSTOM_RNG
+	const uint8_t* random_data;
+	uint32_t random_data_length;
+	uint32_t random_data_index;
+#endif
 } _mr_rng_ctx;
+
+
+#ifdef CUSTOM_RNG
+mr_rng_ctx mr_rng_create_custom(mr_ctx mr_ctx, const uint8_t* random_data, uint32_t random_data_length, uint32_t random_data_index)
+{
+	_mr_rng_ctx* rng = (_mr_rng_ctx*)mr_rng_create(mr_ctx);
+	rng->random_data = random_data;
+	rng->random_data_length = random_data_length;
+	rng->random_data_index = random_data_index;
+	return rng;
+}
+#endif
 
 mr_rng_ctx mr_rng_create(mr_ctx mr_ctx)
 {
@@ -13,8 +35,7 @@ mr_rng_ctx mr_rng_create(mr_ctx mr_ctx)
 	int r = mr_allocate(mr_ctx, sizeof(_mr_rng_ctx), &ctx);
 	if (r != E_SUCCESS) return 0;
 
-	ctx->mr_ctx = mr_ctx;
-	memset(&ctx->rng, 0, sizeof(ctx->rng));
+	*ctx = (_mr_rng_ctx){ mr_ctx };
 	return ctx;
 }
 
@@ -23,6 +44,24 @@ mr_result_t mr_rng_generate(mr_rng_ctx _ctx, uint8_t* output, uint32_t outputsiz
 	_mr_rng_ctx* ctx = _ctx;
 	if (outputsize < 1) return E_INVALIDSIZE;
 	if (!output) return E_INVALIDARGUMENT;
+
+#ifdef CUSTOM_RNG
+	if (ctx->random_data && ctx->random_data_length)
+	{
+		uint32_t amt = outputsize;
+		uint8_t* ptr = output;
+		while (amt > 0)
+		{
+			uint32_t cpy = min(amt, ctx->random_data_length - ctx->random_data_index);
+			memcpy(output, ctx->random_data + ctx->random_data_index, cpy);
+			amt -= cpy;
+			ptr += cpy;
+			ctx->random_data_index = (ctx->random_data_index + cpy) % ctx->random_data_length;
+		}
+
+		return E_SUCCESS;
+	}
+#endif
 
 	if (ctx->rng.seed.handle == 0)
 	{
