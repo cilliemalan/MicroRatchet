8108f5d14361cc0d1e2e3a7efff3968b3529cb43
Cillié Malan <cilliemalan@gmail.com>
2019-10-15 08:09:40 +0200

    Removed overwritten SignatureSize

--- src/CS/MicroRatchet/Interfaces/ISignature.cs
+++ src/CS/MicroRatchet/Interfaces/ISignature.cs
@@ -5,7 +5,6 @@ namespace MicroRatchet
     public interface ISignature : IVerifier
     {
         int PublicKeySize { get; }
-        int SignatureSize { get; }
         byte[] PublicKey { get; }
         byte[] Sign(ArraySegment<byte> data);
     }
