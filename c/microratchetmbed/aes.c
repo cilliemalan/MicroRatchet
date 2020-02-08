a11f17432f2419e4e9ff795a0a0ad47786d3321d
Cillié Malan <cilliemalan@gmail.com>
2020-02-08 13:58:48 +0200

    mbed aes

--- c/microratchetmbed/aes.c
+++ c/microratchetmbed/aes.c
@@ -1,8 +1,10 @@
 #include "pch.h"
 #include <microratchet.h>
+#include <mbedtls/aes.h>
 
 typedef struct {
 	mr_ctx mr_ctx;
+	mbedtls_aes_context aes_ctx;
 } _mr_aes_ctx;
 
 mr_aes_ctx mr_aes_create(mr_ctx mr_ctx)
@@ -21,17 +23,24 @@ mr_result mr_aes_init(mr_aes_ctx _ctx, const uint8_t* key, uint32_t keysize)
 	_mr_aes_ctx* ctx = _ctx;
 	FAILIF(keysize != 16 && keysize != 24 && keysize != 32, MR_E_INVALIDSIZE, "keysize != 16 && keysize != 24 && keysize != 32")
 
-	return MR_E_NOTIMPL;
+	mbedtls_aes_init(&ctx->aes_ctx);
+	int r = mbedtls_aes_setkey_enc(&ctx->aes_ctx, key, keysize * 8);
+	FAILIF(r, MR_E_INVALIDOP, "failed to initialize AES");
+
+	return MR_E_SUCCESS;
 }	
 
 mr_result mr_aes_process(mr_aes_ctx _ctx, const uint8_t* data, uint32_t amount, uint8_t* output, uint32_t spaceavail)
 {
 	_mr_aes_ctx* ctx = _ctx;
-	FAILIF(amount > spaceavail, MR_E_INVALIDSIZE, "amount > spaceavail")
-	FAILIF(!data || !output || !_ctx, MR_E_INVALIDARG, "!data || !output || !_ctx")
-	FAILIF(amount < 16 || spaceavail < 16, MR_E_INVALIDSIZE, "amount < 16 || spaceavail < 16")
+	FAILIF(amount > spaceavail, MR_E_INVALIDSIZE, "amount > spaceavail");
+	FAILIF(!data || !output || !_ctx, MR_E_INVALIDARG, "!data || !output || !_ctx");
+	FAILIF(amount < 16 || spaceavail < 16, MR_E_INVALIDSIZE, "amount < 16 || spaceavail < 16");
 
-	return MR_E_NOTIMPL;
+	int r = mbedtls_internal_aes_encrypt(&ctx->aes_ctx, data, output);
+	FAILIF(r, MR_E_INVALIDOP, "failed to encrypt");
+
+	return MR_E_SUCCESS;
 }
 
 void mr_aes_destroy(mr_aes_ctx ctx)
