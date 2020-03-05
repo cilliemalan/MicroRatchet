daaa57e28cf51b1248fb8c1dbffb172097c8a1c8
Cillié Malan <cilliemalan@gmail.com>
2020-03-05 10:43:28 +0200

    Implemente openssl AES

--- c/microratchetopenssl/aes.c
+++ c/microratchetopenssl/aes.c
@@ -1,8 +1,10 @@
 #include "pch.h"
 #include <microratchet.h>
+#include <openssl/aes.h>
 
 typedef struct {
 	mr_ctx mr_ctx;
+	AES_KEY aes;
 } _mr_aes_ctx;
 
 mr_aes_ctx mr_aes_create(mr_ctx mr_ctx)
@@ -11,7 +13,8 @@ mr_aes_ctx mr_aes_create(mr_ctx mr_ctx)
 	int r = mr_allocate(mr_ctx, sizeof(_mr_aes_ctx), (void**)&ctx);
 	if (r != MR_E_SUCCESS) return 0;
 	*ctx = (_mr_aes_ctx){
-		.mr_ctx = mr_ctx
+		.mr_ctx = mr_ctx,
+		.aes = {0}
 	};
 	return ctx;
 }
@@ -19,19 +22,26 @@ mr_aes_ctx mr_aes_create(mr_ctx mr_ctx)
 mr_result mr_aes_init(mr_aes_ctx _ctx, const uint8_t* key, uint32_t keysize)
 {
 	_mr_aes_ctx* ctx = _ctx;
-	FAILIF(keysize != 16 && keysize != 24 && keysize != 32, MR_E_INVALIDSIZE, "keysize != 16 && keysize != 24 && keysize != 32")
+	FAILIF(!ctx, MR_E_INVALIDARG, "ctx must not be null");
+	FAILIF(!key, MR_E_INVALIDARG, "key must not be null");
+	FAILIF(keysize != 16 && keysize != 24 && keysize != 32, MR_E_INVALIDSIZE, "keysize != 16 && keysize != 24 && keysize != 32");
 
-	return MR_E_NOTIMPL;
-}	
+	int r = AES_set_encrypt_key(key, keysize * 8, &ctx->aes);
+	FAILIF(r != 0, MR_E_INVALIDOP, "Could not set AES key");
+
+	return MR_E_SUCCESS;
+}
 
 mr_result mr_aes_process(mr_aes_ctx _ctx, const uint8_t* data, uint32_t amount, uint8_t* output, uint32_t spaceavail)
 {
 	_mr_aes_ctx* ctx = _ctx;
-	FAILIF(amount > spaceavail, MR_E_INVALIDSIZE, "amount > spaceavail")
-	FAILIF(!data || !output || !_ctx, MR_E_INVALIDARG, "!data || !output || !_ctx")
-	FAILIF(amount < 16 || spaceavail < 16, MR_E_INVALIDSIZE, "amount < 16 || spaceavail < 16")
+	FAILIF(amount > spaceavail, MR_E_INVALIDSIZE, "amount > spaceavail");
+	FAILIF(!data || !output || !_ctx, MR_E_INVALIDARG, "!data || !output || !_ctx");
+	FAILIF(amount < 16 || spaceavail < 16, MR_E_INVALIDSIZE, "amount < 16 || spaceavail < 16");
 
-	return MR_E_NOTIMPL;
+	AES_encrypt(data, output, &ctx->aes);
+
+	return MR_E_SUCCESS;
 }
 
 void mr_aes_destroy(mr_aes_ctx ctx)
