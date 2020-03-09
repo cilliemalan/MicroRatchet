c02a606dc8ebf7ef41a54f2f6a88fd39a60352d6
Cillié Malan <cilliemalan@gmail.com>
2020-03-09 15:15:44 +0200

    FAILIF no longer uses printf

--- c/microratchetwolfssl/pch.h
+++ c/microratchetwolfssl/pch.h
@@ -12,7 +12,17 @@
 
 #ifdef DEBUG
 #include <stdio.h>
-#define FAILIF(condition, error, messageonfailure) if (condition) { printf("%s:%d error: %s\n", __FILE__, __LINE__, messageonfailure); return (error); }
+
+#define STRINGIZE(x) STRINGIZE2(x)
+#define STRINGIZE2(x) #x
+#define LINE_STRING STRINGIZE(__LINE__)
+
+int _write(int file, char *ptr, int len);
+#define _MYWRITE(x) _write((int)stdout, x, sizeof(x))
+
+#define FAILIF(condition, error, messageonfailure) if (condition) { _MYWRITE(__FILE__ ":" LINE_STRING " " messageonfailure); return (error); }
+#define FAILMSG(error, messageonfailure) _MYWRITE(__FILE__ ":" LINE_STRING " " messageonfailure); return (error);
+
 #else
 #define FAILIF(condition, error, messageonfailure) if (condition) { return (error); }
 #endif
