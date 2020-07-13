02b84ef2f5b6a0c64c23c6f6c0462eaf5194ba4d
Cillié Malan <cilliemalan@gmail.com>
2020-07-13 17:29:46 +0200

    Fixed aesctr memset bug

--- c/libmicroratchet/aesctr.c
+++ c/libmicroratchet/aesctr.c
@@ -8,7 +8,7 @@ mr_result aesctr_init(_mr_aesctr_ctx *ctx, mr_aes_ctx aes, const uint8_t *iv, ui
 	FAILIF(!iv || !ctx || !aes, MR_E_INVALIDARG, "!iv || !ctx || !aes");
 
 	ctx->aes_ctx = aes;
-	mr_memcpy(ctx->ctr, 0, sizeof(ctx->ctr));
+	mr_memzero(ctx->ctr, sizeof(ctx->ctr));
 	mr_memcpy(ctx->ctr, iv, ivsize > 16 ? 16 : ivsize);
 	ctx->ctrix = 0;
 	
