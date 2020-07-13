0bd943c15d77ff8a95334186f8afc612a9fceb73
Cillié Malan <cilliemalan@gmail.com>
2020-07-13 10:40:27 +0200

    Added rng test GeneratePadded

--- c/microratchettests/rng.cpp
+++ c/microratchettests/rng.cpp
@@ -33,6 +33,45 @@ TEST(Rng, Generate) {
 	mr_ctx_destroy(mr_ctx);
 }
 
+TEST(Rng, GeneratePadded) {
+	uint8_t data[64];
+	memset(data, 0xCC, sizeof(data));
+
+	auto mr_ctx = mr_ctx_create(&_cfg);
+	auto rng = mr_rng_create(mr_ctx);
+	int result = mr_rng_generate(rng, data + 16, 28);
+	EXPECT_EQ(MR_E_SUCCESS, result);
+
+	bool allcc = true;
+	for (int i = 16; i < 44; i++)
+	{
+		if (data[i] != 0xCC)
+		{
+			allcc = false; break;
+		}
+	}
+	EXPECT_FALSE(allcc);
+	allcc = true;
+	for (int i = 0; i < 16; i++)
+	{
+		if (data[i] != 0xCC)
+		{
+			allcc = false; break;
+		}
+	}
+	for (int i = 44; i < 64; i++)
+	{
+		if (data[i] != 0xCC)
+		{
+			allcc = false; break;
+		}
+	}
+	EXPECT_TRUE(allcc);
+
+	mr_rng_destroy(rng);
+	mr_ctx_destroy(mr_ctx);
+}
+
 TEST(Rng, GenerateTwice) {
 	uint8_t data1[32];
 	uint8_t data2[32];
